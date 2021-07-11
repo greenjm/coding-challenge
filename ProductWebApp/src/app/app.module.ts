6b8949c36640460c38209e821fe846e796112c42
jmgreen <jmgreen@sep.com>
2021-07-10 23:07:34 -0400

    Introduced routing to front-end app

--- ProductWebApp/src/app/app.module.ts
+++ ProductWebApp/src/app/app.module.ts
@@ -6,6 +6,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ListProductsComponent } from './list-products/list-products.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: "list",
+    component: ListProductsComponent
+  },
+  {
+    path: "create",
+    component: AddProductComponent
+  },
+];
 
 @NgModule({
   declarations: [
@@ -18,6 +30,7 @@ import { ListProductsComponent } from './list-products/list-products.component';
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
